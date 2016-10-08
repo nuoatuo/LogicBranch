bdfa37d8355368254ae5ae1528bbf64f1a223f63
nuoatuo <nuoatuo@126.com>
2016-10-08 14:30:11 +0800

    guard的使用
    guard的使用

--- LogicBranch.playground/Contents.swift
+++ LogicBranch.playground/Contents.swift
@@ -17,6 +17,7 @@ import UIKit
         2>没有非0(nil)即真 
             Bool(true/false)
  */
+/*
 let a = 10
 if a > 0 {
     print("a大于0")
@@ -38,4 +39,66 @@ if score < 0 || score > 100 {
 } else {
     print("优秀")
 }
+*/
 
+//======== guard的使用 ==========
+/**
+ guard是Swift2.0新增的语法
+ 它与if语句非常类似，它设计的目的是提高程序的可读性
+ guard语句必须带有else语句，它的语法如下：
+    1>当条件表达式为true时跳过else语句中的内容，执行语句组内容
+    2>条件表达式为false时候执行else语句中的内容，跳转语句一般是return、break、continue 和 throw
+ 
+    guard 条件表达式 else {
+        //条换语句
+        break
+    }
+    语句组
+ */
+
+//年龄是20，判断该人能不能在网吧上网
+let age = 20
+
+//swif方法(函数)
+/*
+func online(age: Int) {
+    if age > 18 {
+        if  带了身份证 {
+            if 带了钱 {
+                print("可以留下来上网")
+            } else {
+                print("回家拿钱")
+            }
+        } else {
+            print("回家拿身份证")
+        }
+    } else {
+        print("回家去")
+    }
+}
+online(age: age)
+
+func online(age: Int) {
+    //1.判断年龄是否大于18岁
+    guard age >= 18 else {
+        print("回家去")
+        return
+    }
+    
+    //2.判断是否带了身份证
+    guard 带了身份证 else {
+        print("回家拿身份证")
+        return
+    }
+    
+    //3.判断是否带了钱
+    guard 带了身份证 else {
+        print("回家拿钱")
+        return
+    }
+    
+    print("可以留下来上网")
+}
+ */
+
+//======== switch的使用 ==========
